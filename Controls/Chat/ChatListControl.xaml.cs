8966f553d8ee5a8461b1fd1d46ef58a09e41f114
Megafont <51346737+Megafont@users.noreply.github.com>
2024-04-22 18:52:28 -0500

    Finished video #10: "ItemsControl Chat List & Design Time Data"

---
+++ Controls/Chat/ChatListControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ASPNet_WPF_ChatApp.Controls.Chat
+{
+    /// <summary>
+    /// Interaction logic for ChatListControl.xaml
+    /// </summary>
+    public partial class ChatListControl : UserControl
+    {
+        public ChatListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
