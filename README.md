aa03a43317ce8d433fe74d220de013a9912abb31
Megafont <51346737+Megafont@users.noreply.github.com>
2024-05-14 14:49:51 -0500

    Finished web server video #2: "SQL Server Basics Learn Transact-SQL Management Studio"

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # ASPNet-WPF-ChatApp
 
 This project is chat app using the MVVM  pattern (Model-View-ViewModel), which is a refinement of the MVC pattern (Model-View-Controller).
-It was created by following this WPF/ASP.Net tutorial series on YouTube, starting at video #4: "C# WPF UI Tutorials: 04 - Custom Window Chrome and Styles" 
+It was created by following AngelSix's WPF/ASP.Net tutorial series on YouTube, starting at video #4: "C# WPF UI Tutorials: 04 - Custom Window Chrome and Styles" 
 in this [playlist](https://www.youtube.com/watch?v=TDOxHx-AMqQ&list=PLrW43fNmjaQVYF4zgsD0oL9Iv6u23PI6M&index=4).
 
-Also, here is [his repo](https://github.com/angelsix/fasetto-word/tree/dc1d288472211d8d0e7b7baf580e51dfb6552b6f) for this project
+The backend web server for this project was built by following his MVC [playlist](https://www.youtube.com/playlist?list=PLrW43fNmjaQUBZv0OiliNY4fStb4Vj1u4).
+
+For both playlists, each video has a link to the source code for that video on Github.
---
+++ SqlConnector/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlConnector
+{
+    /// <summary>
+    /// This project is a simple test app that acccesses the database called Test, which we created in video 2 of the MVC playlist:
+    /// https://www.youtube.com/watch?v=EI30Zl40IhQ&list=PLrW43fNmjaQUBZv0OiliNY4fStb4Vj1u4&index=3
+    /// 
+    /// This project demonstrates how to access and manipulate a database in C# with an <see cref="SqlConnection"/>.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var sqlConnection = new SqlConnection("Server=.; Database=Test; User Id=TestUser; Password=TestUser"))
+            {
+                sqlConnection.Open();
+
+
+                using (var command = new SqlCommand($"INSERT INTO dbo.Users (Id, Username, FirstName, LastName, IsEnabled, CreatedDateUtc) VALUES ('{Guid.NewGuid().ToString("N")}', 'Username1', 'FirstName1', 'LastName1', 1, '2024-05-14 14:42:00.0000000 -06:00')", sqlConnection))
+                {
+                    var result = command.ExecuteNonQuery();
+
+                }
+
+
+                using (var command = new SqlCommand("SELECT * FROM Users", sqlConnection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"Username {reader["Username"]}, First Name: {reader["FirstName"]}, LastName: {reader["LastName"]}, IsEnabled? {reader["IsEnabled"]}");
+                        }
+                    }
+
+                }
+
+                sqlConnection.Close();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to close...");
+            Console.Read();
+        }
+    }
+}
